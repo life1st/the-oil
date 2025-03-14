e7cbba8827641629d0f4158233c4359812f62b40
jiaoyang18 <jiaoyang18@maoyan.com>
2025-03-14 17:03:11 +0800

    feat: 优化同步设置

--- src/components/sync-setting/index.tsx
+++ src/components/sync-setting/index.tsx
@@ -5,168 +5,213 @@ import useRecordStore from '@/store/recordStore'
 import gistSync from '@/utils/sync/gist-provider'
 import './style.scss'
 import dayjs from 'dayjs'
+import { DEFAULT_VEHICLE_ID, DEFAULT_VEHICLE_NAME } from '../../utils/consts'
+
+const withLoading = async (fn: (...args: any) => Promise<boolean> ) => {
+  Toast.show({
+    icon: 'loading',
+    content: '同步中...',
+    duration: 0
+  })
+  const result = await fn()
+  if (result) {
+    Toast.show({
+      icon: 'success',
+      content: '同步成功'
+    })
+  } else {
+    Toast.show({
+      icon: 'fail',
+      content: '同步失败，请检查配置'
+    })
+  }
+}
 
 const SyncSetting: FC = () => {
-  const [visible, setVisible] = useState(false)
   const [form] = Form.useForm<GistConfig>()
-  const { gistConfig, setGistConfig, syncTime, updateSyncTime } = useSettingStore()
+  const { 
+    gistConfig, 
+    setGistConfig, 
+    syncTime, 
+    updateSyncTime,
+    vehicleId,
+    setVehicleId
+  } = useSettingStore()
   const { mergeRecordData, recordList } = useRecordStore()
-  const { token, gistId, filename } = gistConfig || {}
+  const [files, setFiles] = useState(null)
 
+  const { token, gistId } = gistConfig || {}
+
+  useEffect(() => {
+    const fetchInit = async () => {
+      const response = await gistSync.get(gistId)
+      const files = response.data?.files
+      setFiles(files)
+    }
+    fetchInit()
+  }, [])
   useEffect(() => {
     if (token) {
       gistSync.setToken(token)
     }
   }, [token])
-  const handleSync = async () => {
-    if (!gistConfig) {
-      setVisible(true)
-      return
-    }
 
-    try {
-      Toast.show({
-        icon: 'loading',
-        content: '同步中...',
-        duration: 0
-      })
 
-      const response = await gistSync.get(gistId)
-
-      const recordList = JSON.parse(response.data?.files[filename]?.content || '')
-      if (!recordList) {
-        throw new Error('同步失败')
-      }
-
-      mergeRecordData(recordList)
-      
-      Toast.show({
-        icon: 'success',
-        content: '同步成功'
-      })
-      updateSyncTime()
-    } catch (error) {
-      Toast.show({
-        icon: 'fail',
-        content: '同步失败，请检查配置'
-      })
-    }
-  }
-
-  const handleSyncTo = async () => {
-    try {
-      const recordListJson = JSON.stringify(recordList)
-      const response = await gistSync.update(gistId, {
-        filename, content: recordListJson
-      })
-      if (response.status === 200) {
+  const showGistConfigModal = () => {
+    const handleSubmit = async () => {
+      let values = null
+      try {
+        values = await form.validateFields()
         Toast.show({
           icon: 'success',
-          content: '同步成功'
+          content: '配置已保存'
         })
-        updateSyncTime()
+      } catch (error) {
+        Toast.show({
+          icon: 'fail',
+          content: '请填写完整信息'
+        })
       }
-    } catch {
-      Toast.show({
-        icon: 'fail',
-        content: '同步失败，请检查配置'
-      })
+      if (!values) {
+        return
+      }
+      setGistConfig(values)
     }
+    Modal.confirm({
+      title: 'Gist 同步设置',
+      content: (
+        <Form
+          form={form}
+          initialValues={gistConfig}
+          layout='vertical'
+          className='sync-form'
+        >
+          <Form.Item
+            label='Personal Access Token'
+            name='token'
+            rules={[{ required: true, message: '请输入 Token' }]}
+          >
+            <Input placeholder='请输入 GitHub Token' />
+          </Form.Item>
+          <Form.Item
+            label='Gist ID'
+            name='gistId'
+            rules={[{ required: true, message: '请输入 Gist ID' }]}
+          >
+            <Input placeholder='请输入 Gist ID' />
+          </Form.Item>
+        </Form>
+      ),
+      onConfirm: () => {
+        handleSubmit()
+      }
+    })
   }
 
-  const handleSubmit = async () => {
-    let values = null
-    try {
-      values = await form.validateFields()
-      Toast.show({
-        icon: 'success',
-        content: '配置已保存'
-      })
-      setVisible(false)
-    } catch (error) {
-      Toast.show({
-        icon: 'fail',
-        content: '请填写完整信息'
-      })
-    }
-    if (!values) {
+  const handleShowSyncModal = async () => {
+    if (!gistConfig) {
+      showGistConfigModal()
       return
     }
-    setGistConfig(values)
+
+    const handleSync = () => {
+      if (!gistConfig) {
+        showGistConfigModal()
+        return
+      }
+
+      withLoading(async () => {
+        try {
+          const response = await gistSync.get(gistId)
+          const files = response.data?.files
+          const recordList = JSON.parse(files[vehicleId]?.content || '')
+          if (!recordList) {
+            throw new Error('同步失败')
+          }
+
+          mergeRecordData(recordList)
+          
+          updateSyncTime()
+          return true
+        } catch (error) {
+          return false
+        }
+      })
+    }
+    const handleSyncTo = () => {
+      withLoading(async () => {
+        try {
+          const recordListJson = JSON.stringify(recordList)
+          const response = await gistSync.update(gistId, {
+            filename: vehicleId, content: recordListJson
+          })
+          if (response.status === 200) {
+            updateSyncTime()
+          }
+          return response.status === 200
+        } catch {
+          return false
+        }
+      })
+    }
+
+    Modal.alert({
+      title: 'Gist 同步',
+      content: (
+        <List>
+          <List.Item onClick={showGistConfigModal}>Gist 配置</List.Item>
+          {
+            gistConfig ? (
+              <>
+              <List.Item onClick={handleSync}>从 Gist 下载</List.Item>
+              <List.Item onClick={handleSyncTo}>同步到 Gist</List.Item>
+              </>
+            ) : null
+          }
+        </List>
+      )
+    })
+  }
+
+  const changeVehicle = async () => {
+    Modal.alert({
+      title: '切换车辆',
+      content: (
+        <List>
+          {Object.keys(files).map((filename) => (
+            <List.Item key={filename}> 
+              {filename === DEFAULT_VEHICLE_ID 
+              ? DEFAULT_VEHICLE_NAME 
+              : filename}
+            </List.Item>
+          ))}
+        </List>
+      )
+    })
   }
 
   return (
     <>
       <List.Item
-        onClick={handleSync}
-        extra={gistConfig ? '点击同步' : '未配置'}
+        onClick={handleShowSyncModal}
+        extra={gistConfig ? '' : '未配置'}
         description={syncTime 
           ? `上次同步时间: ${dayjs(syncTime).format('YY/MM/DD - HH:mm')}` 
           : ''
         }
       >
-        {gistConfig ? '从 Gist 同步' : 'Gist 同步'}
+        Gist 同步
       </List.Item>
       {gistConfig && (
-        <List.Item
-          onClick={handleSyncTo}
-          extra="点击同步"
-        >
-          同步到 Gist
-        </List.Item>
-      )}
-
-      <Modal
-        visible={visible}
-        title='Gist 同步设置'
-        content={
-          <Form
-            form={form}
-            layout='vertical'
-            className='sync-form'
+        <>
+          <List.Item 
+            onClick={changeVehicle}
+            extra={vehicleId === DEFAULT_VEHICLE_ID ? '默认车辆' : vehicleId}
           >
-            <Form.Item
-              label='Personal Access Token'
-              name='token'
-              rules={[{ required: true, message: '请输入 Token' }]}
-            >
-              <Input placeholder='请输入 GitHub Token' />
-            </Form.Item>
-            <Form.Item
-              label='文件名'
-              name='filename'
-              rules={[{ required: true, message: '请输入文件名' }]}
-            >
-              <Input placeholder='例如：records.json' />
-            </Form.Item>
-            <Form.Item
-              label='Gist ID'
-              name='gistId'
-              rules={[{ required: true, message: '请输入 Gist ID' }]}
-            >
-              <Input placeholder='请输入 Gist ID' />
-            </Form.Item>
-          </Form>
-        }
-        closeOnAction
-        onClose={() => setVisible(false)}
-        actions={[
-          {
-            key: 'cancel',
-            text: '取消'
-          },
-          {
-            key: 'confirm',
-            text: '确认',
-            primary: true
-          }
-        ]}
-        onAction={(action) => {
-          if (action.key === 'confirm') {
-            handleSubmit()
-          }
-        }}
-      />
+            切换车辆
+          </List.Item>
+        </>
+      )}
     </>
   )
 }
--- src/store/setting-store.ts
+++ src/store/setting-store.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware'
+import { DEFAULT_VEHICLE_ID } from '@/utils/consts';
 
 export interface GistConfig {
   token: string
@@ -8,15 +9,19 @@ export interface GistConfig {
 }
 
 interface SyncStore {
-    syncTime: number | null
-    gistConfig: GistConfig | null
-    setGistConfig: (config: GistConfig) => void
-    clearAllConfig: () => void
+  vehicleId: string;
+  setVehicleId: (id: string) => void;
+  syncTime: number | null
+  gistConfig: GistConfig | null
+  setGistConfig: (config: GistConfig) => void
+  clearAllConfig: () => void
 }
 
 const useSettingStore = create<SyncStore>()(
   persist(
     (set) => ({
+        vehicleId: DEFAULT_VEHICLE_ID,
+        setVehicleId: (id: string) => set({ vehicleId: id }),
         syncTime: null,
         updateSyncTime: () => set({ syncTime: Date.now() }),
         gistConfig: null,
