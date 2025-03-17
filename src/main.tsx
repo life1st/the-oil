712d48e11cb2eae617e547f869e47fe4cf839903
jiaoyang18 <jiaoyang18@maoyan.com>
2025-03-17 11:32:48 +0800

    fix: darkmode 适配

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/main.scss'
 import Router from './router'
+import { auto } from 'darkreader'
 
+auto({
+    brightness: 100,
+    contrast: 90,
+    sepia: 10,
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
