c6a0b73e731da0b04ebd4297db480c29fae2d152
jiaoyang18 <jiaoyang18@maoyan.com>
2025-03-24 17:12:32 +0800

    feat: ut ci

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
     globals: true,
     setupFiles: ['./src/test/setup.ts'],
     coverage: {
-      provider: 'v8',
+      provider: 'istanbul',
       reporter: ['text', 'html'],
     },
   },
